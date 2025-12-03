8957e3a4afbc58e2eb3080889b1d5de6267e821a
Frederik Lauber <acc-github-frederik-lauber@flambda.de>
2025-12-03 01:57:37 +0100

    CHG: autodetect the system python version to link correctly for our tests

--- py/build.rs
+++ py/build.rs
@@ -1,7 +1,15 @@
+use std::process::Command;
+
 fn main() {
-    #[cfg(test)]
-    {
-        println!("cargo:rustc-link-lib=python3.12");
-        println!("cargo:rustc-link-search=native=/usr/lib");
-    }
+    let output = Command::new("python3")
+        .arg("-c")
+        .arg("import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
+        .output()
+        .expect("Failed to run python3");
+
+    let version = String::from_utf8(output.stdout).unwrap();
+    let version = version.trim();
+
+    println!("cargo:rustc-link-lib=dylib=python{}", version);
+    println!("cargo:rustc-link-search=native=/usr/lib");
 }
